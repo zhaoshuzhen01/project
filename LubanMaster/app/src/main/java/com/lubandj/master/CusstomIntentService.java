02c8a664fbf310429537370d6919ecce54d4fb2d
zhaoshuzhen01 <zsz521@126.com>
2017-12-05 13:57:07 +0800

    添加个推

--- LubanMaster/app/src/main/java/com/lubandj/master/CusstomIntentService.java
+++ LubanMaster/app/src/main/java/com/lubandj/master/CusstomIntentService.java
@@ -1,12 +1,17 @@
 package com.lubandj.master;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 
 import com.igexin.sdk.GTIntentService;
 import com.igexin.sdk.PushManager;
 import com.igexin.sdk.message.GTCmdMessage;
 import com.igexin.sdk.message.GTTransmitMessage;
+import com.lubandj.master.worksheet.WorkSheetListActivity;
 
 /**
  * Created by ${zhaoshuzhen} on 2017/12/3.
@@ -37,25 +42,14 @@ public class CusstomIntentService extends GTIntentService {
 
         // 第三方回执调用接口，actionid范围为90000-90999，可根据业务场景执行
         boolean result = PushManager.getInstance().sendFeedbackMessage(context, taskid, messageid, 90001);
-        Log.d(TAG, "call sendFeedbackMessage = " + (result ? "success" : "failed"));
 
-        Log.d(TAG, "onReceiveMessageData -> " + "appid = " + appid + "\ntaskid = " + taskid + "\nmessageid = " + messageid + "\npkg = " + pkg
-                + "\ncid = " + cid);
 
         if (payload == null) {
             Log.e(TAG, "receiver payload = null");
         } else {
             String data = new String(payload);
-            Log.d(TAG, "receiver payload = " + data);
-
-            // 测试消息为了观察数据变化
-            if (data.equals("收到一条透传测试消息")) {
-                data = data + "-" + cnt;
-                cnt++;
-            }
+            shownotification(data,context);
         }
-
-        Log.d(TAG, "----------------------------------------------------------------------------------------------");
     }
 
     @Override
@@ -70,5 +64,56 @@ public class CusstomIntentService extends GTIntentService {
     @Override
     public void onReceiveCommandResult(Context context, GTCmdMessage cmdMessage) {
         Log.e("deal",cmdMessage.getAppid()+"");
+    }
+    // 推送通知栏设置
+    public void shownotification(String msg,Context context) {
+
+        // 消息存储
+//        MeBill meBill = new Gson().fromJson(msg, MeBill.class);
+       /* SQLiteDatabase w_db = FaceDataBase.getInstance()
+                .getFaceWritableDatabase(context,
+                        FaceDataBaseHelper.MESSAGE_TABLE_NAME);
+        meBill.setIsRead("0");
+        meBill.setUpdate_time(System.currentTimeMillis() + "");
+        meBill.setPhoneNum(UserMsg.getInstance().getD_id());
+        FaceDataBase.getInstance().insertMsg(w_db, meBill);
+
+        BusProvider.getInstance().post(
+                new DataChangedEvent(BusAction.CHANGED_MSG_DATABASE));*/
+        // 点击事件设置
+        Intent appIntent = new Intent(Intent.ACTION_MAIN);
+        appIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        appIntent.setClass(context, WorkSheetListActivity.class);
+        appIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+
+       /* Bundle bundle = new Bundle();
+        bundle.putSerializable(Intents.MSG_OBJECT, meBill);
+        appIntent.putExtras(bundle);*/
+
+        // 通知生成
+        NotificationManager barmanager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        int icon = R.mipmap.ic_launcher;
+
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
+                appIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification.Builder builder = new Notification.Builder(context);
+        builder.setSmallIcon(icon).setTicker("您有新的消息要查看");
+
+        String title = msg;
+        String text = msg;
+        builder.setContentTitle(title).setContentText(text)
+                .setContentIntent(contentIntent);
+
+        Notification notice = builder.getNotification();
+        notice.flags = Notification.FLAG_AUTO_CANCEL;
+        notice.defaults |= Notification.DEFAULT_SOUND;// 声音
+        // messageNotification.defaults |= Notification.DEFAULT_LIGHTS;//灯
+        notice.defaults |= Notification.DEFAULT_VIBRATE;// 震动
+
+        barmanager.notify(System.currentTimeMillis() + "", 2008, notice);
+
     }
 }
