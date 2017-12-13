a584f20fce61cd0b545fa6d578b92eddc61de19e
zhaoshuzhen01 <zsz521@126.com>
2017-12-13 21:30:35 +0800

    /

--- LubanMaster/app/src/main/java/com/lubandj/master/customview/DayShowView.java
+++ LubanMaster/app/src/main/java/com/lubandj/master/customview/DayShowView.java
@@ -1,4 +1,4 @@
-package com.lubandj.master.my;
+package com.lubandj.master.customview;
 
 import android.content.Context;
 import android.graphics.Color;
--- LubanMaster/app/src/main/java/com/lubandj/master/my/AboutLuBanActivity.java
+++ LubanMaster/app/src/main/java/com/lubandj/master/my/AboutLuBanActivity.java
@@ -1,12 +1,17 @@
 package com.lubandj.master.my;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 
 import com.example.baselibrary.BaseActivity;
+import com.example.baselibrary.tools.ToastUtils;
+import com.example.baselibrary.tools.Tools;
 import com.lubandj.master.R;
 import com.lubandj.master.databinding.ActivityAboutlubanBinding;
 
@@ -24,6 +29,9 @@ public class AboutLuBanActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_aboutluban);
+
+        String versionName = Tools.getVersionName(AboutLuBanActivity.this);
+        binding.tvVersion.setText("当前版本: " + versionName);
     }
 
     @Override
@@ -53,5 +61,43 @@ public class AboutLuBanActivity extends BaseActivity {
      */
     public void onBack(View view) {
         finish();
+    }
+
+
+    /**
+     * 检查更新
+     *
+     * @param view
+     */
+    public void onUpdate(View view) {
+        ToastUtils.showShort(AboutLuBanActivity.this, "点击更新");
+    }
+
+
+    /**
+     * 用户协议
+     *
+     * @param view
+     */
+    public void onUserAggrement(View view) {
+        Intent intent = new Intent(AboutLuBanActivity.this, AgreementActivity.class);
+        String url = "http://www.baidu.com/";
+        intent.putExtra("url", url);
+        startActivity(intent);
+    }
+
+    /**
+     * 去评分
+     *
+     * @param view
+     */
+    public void onGoRate(View view) {
+        Uri uri = Uri.parse("market://details?id=" + getPackageName());
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        try {
+            startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            ToastUtils.showShort(AboutLuBanActivity.this, "尚未安装应用市场，无法评分");
+        }
     }
 }
